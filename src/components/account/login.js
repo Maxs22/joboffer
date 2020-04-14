c60a4f1afdaa323fb367f010af58094a07a92a90
Diego Camacho <dcamacho31@hotmail.com>
2020-04-14 18:49:23 -0300

    #Adding Hooks to login form to compare against class component

--- src/components/account/login.js
+++ src/components/account/login.js
@@ -1,53 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './job.jpg';
 
-class Login extends React.Component
-{
-    constructor(props)
-    {
-        super(props)
-        this.state = {
-            loginStatus: 'error',
-            errorMessage: ''
-        };
-    }
+export default function Login(props){
 
-    style = {
+    const [user, setUser] = useState();
+    const [password, setPassword] = useState();
+    const [errorMessage, setErrorMessage] = useState();
+    const [loginStatus, setloginStatus] = useState();
+
+    const style = {
         form:{
             marginTop: '50px'
         }
     }
 
-    render(){
+    const handleUserChange = (event) => {
+        setUser(event.target.value);
+    }
 
-        const errorMessage = this.state.loginStatus === 'error' &&
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const signIn = (event) =>{
+        event.preventDefault();
+        if(user !== 'a@b.com'){
+            setErrorMessage('Usuario no permitido para ingresar');
+            setloginStatus('error');
+        }
+        else
+        {
+            setloginStatus('ok');
+        }
+    }
+
+    const errorMessageTitle = loginStatus === 'error' &&
                                     ( <div class="alert alert-danger" role="alert">
-                                            This is a danger alert—check it out!
+                                            {errorMessage}
                                     </div>)
 
-
-        return (
-            <div className="row" style={this.style.form}>
+    return (
+        <div className="row" style={style.form}>
                 <div className="offset-1 col-10 offset-1 offset-sm-3 col-sm-6 offset-sm-3 offset-md-4 col-md-4 offset-md-4 offset-xl-5 col-xl-2 offset-xl-5">
                     <form className="form-signing">
                         <img  className="mb-5" src={logo} alt="Job Offers logo"/>
-                        {errorMessage}
+                        {errorMessageTitle}
                         <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
                         <label htmlFor="inputEmail" className="sr-only">Email address</label>
-                        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required="" autoFocus=""/>
+                        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" onChange={handleUserChange} required="" autoFocus=""/>
                         <label htmlFor="inputPassword" className="sr-only">Password</label>
-                        <input type="password" id="inputPassword" className="form-control mt-3" placeholder="Password" required=""></input>
+                        <input type="password" id="inputPassword" className="form-control mt-3" placeholder="Password" onChange={handlePasswordChange} required=""></input>
                         <div className="checkbox mb-3 mt-3">
                             <label>Remember me
                                 <input type="checkbox" className="ml-3" value="remember-me"/>
                             </label>
                         </div>
-                        <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                        <button className="btn btn-lg btn-primary btn-block" onClick={signIn}>Sign in</button>
                     </form>
                 </div>
             </div>
-        )
-    }
+    )
 }
-
-export default Login
