e51b7879e7e6ba9106372380479f064d282b9568
Diego Camacho <dcamacho31@hotmail.com>
2020-06-17 23:54:39 -0300

    Moving responsibilities from JobList to JobListPage

--- src/components/jobsList/jobsList.jsx
+++ src/components/jobsList/jobsList.jsx
@@ -1,71 +1,18 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import { Container } from 'react-bootstrap';
-import { jobListLoading, jobListLoaded, jobListLoadingError } from '../../redux/jobListState/jobListActions';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import JobsListItem from '../jobsListItem/jobsListItem';
-import Loader from 'react-loader-spinner'
+import { useSelector } from 'react-redux';
+
 
 export default function JobsList() {
 
-    const dispatch = useDispatch();
-
-    const showLoadingSpinner = useSelector(state => state.jobListState.jobListLoading);
-    const jobOfferLoadingError = useSelector(state => state.jobListState.jobListLoadingError);
-
     let jobs = useSelector(state => state.jobListState.jobListObjects);
 
-    useEffect(()=>{
-
-        const fetchJobs = async () =>{
-
-            const data = await fetch('http://localhost:61256/api/jobsoffer',{
-                mode: 'cors'
-            })
-            .catch(function(error) {
-                dispatch(jobListLoadingError);
-              });
-
-            if(typeof data !== "undefined" ){
-
-                const json = await data.json();
-
-                dispatch(jobListLoaded(json));
-            }
-
-        }
-
-        if ((typeof jobs === 'undefined' || jobs.length === 0) && !jobOfferLoadingError){
-
-            dispatch(jobListLoading);
-
-            fetchJobs();
-        }
-
-    }, [jobs, dispatch, jobOfferLoadingError]);
-
-    let jobItems = jobs.length === 0 && !showLoadingSpinner ? <p>No hay ofertas de trabajo</p> : "";
-
-    if(jobs.length > 0)
-    {
-        jobItems = jobs.map(item => <JobsListItem key={item.id}  job={ item }></JobsListItem>);
-    }
-
-    const spinner = showLoadingSpinner && (
-        <span>
-            <Loader
-                type="TailSpin"
-                color="black"
-                height={100}
-                width={100} //3 secs
-            />
-            <p>Cargando...</p>
-        </span>
-       );
+    let jobItems = jobs.length === 0 ? <p>No hay ofertas de trabajo</p> : jobs.map(item => <JobsListItem key={item.id}  job={ item }></JobsListItem>);
 
     return (
         <Container>
-            { spinner }
             { jobItems }
         </Container>
     );
