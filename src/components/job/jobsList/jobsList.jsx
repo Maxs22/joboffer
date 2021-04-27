af239777e5d85056c51e89e9703e5215da1538f6
Diego Camacho <dcamacho31@hotmail.com>
2021-04-27 11:25:00 -0300

    Bug fix: spinner was not working when loading job list

--- src/components/job/jobsList/jobsList.jsx
+++ src/components/job/jobsList/jobsList.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 import "../../../../node_modules/react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import JobsListItem from '../../../components/job/jobsListItem/jobsListItem';
 export default function JobsList(props) {
 
-    
+    const isJobListLoading = useSelector(state => state.JobListState.jobListLoading);
 
-    let jobItems = props.jobs.length === 0 ? <p>No hay ofertas de trabajo</p> : props.jobs.map(item => <JobsListItem key={item.jobOffer.id}  jobOffer={ item.jobOffer } isRecruiter={props.isRecruiter}></JobsListItem>);
+    let jobItems = props.jobs.length === 0 && !isJobListLoading ? <p>No hay ofertas de trabajo</p> : props.jobs.map(item => <JobsListItem key={item.jobOffer.id}  jobOffer={ item.jobOffer } isRecruiter={props.isRecruiter}></JobsListItem>);
 
     return (
         <Container>
