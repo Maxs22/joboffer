511cbd25401c6da7a3169fa9db4b3b5f7530cac0
Diego Camacho <dcamacho31@hotmail.com>
2021-04-30 12:26:27 -0300

    keep working

---
+++ src/redux/job/editCreateJobReducer.js
@@ -0,0 +1,54 @@
+const initialState = {
+    title: '',
+    description: '',
+    companyActivity: '',
+    companyName: '',
+    date: '',
+    isLanguageMandatoryControl: false,
+    kindOfContract: '',
+    language: '',
+    languageLevelControl: 0,
+    skills: '',
+    startingFrom: '',
+    workingDays: '',
+    zone: ''
+
+}
+
+function getSkillsRequired(skills){
+    
+}
+
+export function EditCreateJobReducer(state = initialState, action) {
+
+    switch (action.type) {
+        case 'CLEAN': return {
+            ...state
+        };
+        case 'SAVE': return {
+            title: action.payload.title,
+            description: action.payload.description,
+            company: {
+                name:    action.payload.companyName,
+                activity:  action.payload.companyActivity
+            },
+            date: action.payload.date,
+            isLanguageMandatory: action.payload.isLanguageMandatoryControl,            
+            language: action.payload.language,
+            languageLevel: action.payload.languageLevelControl,
+            skillsRequired: [],
+            zone: action.payload.workingDays,
+            contractInformation: {
+                startingFrom: action.payload.startingFrom,
+                workingDays: action.payload.workingDays,
+                kindOfContract: action.payload.kindOfContract,
+            }
+            
+            
+            
+            
+        }
+        default: return state;
+    }
+
+}
